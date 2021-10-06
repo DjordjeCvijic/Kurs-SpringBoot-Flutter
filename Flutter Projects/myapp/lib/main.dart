12f648aeda5effcba5f6b8f175d22b79ebc13e6c
DjordjeCvijic <djordjecvijic96@gmail.com>
2021-10-06 12:48:10 +0200

    U flutter app uradjena login forma

--- Flutter Projects/myapp/lib/main.dart
+++ Flutter Projects/myapp/lib/main.dart
@@ -13,23 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var _controller = TextEditingController();
+
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
             primarySwatch: Colors.green,
             buttonTheme: const ButtonThemeData(buttonColor: Colors.red)),
         home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Naslov"),
-          ),
+          backgroundColor: Colors.grey[850],
           body: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Flexible(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.red,
-                  )),
+              Flexible(flex: 1, child: Container()),
               Flexible(
                   flex: 3,
                   child: Column(
@@ -39,15 +34,21 @@ class MyApp extends StatelessWidget {
                       const Text(
                         "Login",
                         style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
-                      const SizedBox(height: 10),
+                      const SizedBox(height: 20),
                       const Text(
                         "Email",
                         style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
+                      const SizedBox(height: 7),
                       TextField(
+                        cursorColor: Colors.black,
                         decoration: InputDecoration(
                             filled: true,
                             fillColor: Colors.grey.shade300,
@@ -67,9 +68,13 @@ class MyApp extends StatelessWidget {
                       const Text(
                         "Password",
                         style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
                       ),
+                      const SizedBox(height: 7),
                       TextField(
+                        cursorColor: Colors.black,
                         controller: _controller,
                         decoration: InputDecoration(
                           filled: true,
@@ -84,29 +89,46 @@ class MyApp extends StatelessWidget {
                           hintText: 'Password',
                           hintStyle: const TextStyle(fontSize: 12),
                           suffixIcon: IconButton(
+                              color: Colors.black,
                               onPressed: _controller.clear,
                               icon: const Icon(Icons.visibility)),
                         ),
                       ),
-                      const SizedBox(height: 3),
+                      const SizedBox(height: 8),
                       RichText(
                         text: TextSpan(children: [
                           TextSpan(
-                              text: "Forgot password ?",
-                              style: const TextStyle(fontSize: 8),
+                              text: "Forgot passweord ?",
+                              style: const TextStyle(
+                                  fontSize: 10, color: Colors.white),
                               recognizer: TapGestureRecognizer()
                                 ..onTap = () {
                                   print('Forgot password Clicked');
                                 }),
                         ]),
                       ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.blue),
+                              minimumSize: MaterialStateProperty.all(
+                                  const Size(double.infinity, 50)),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(28.0),
+                              ))),
+                          onPressed: () {
+                            print("Login Button ");
+                          },
+                          child: const Text('Log in',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                              )))
                     ],
                   )),
-              Flexible(
-                  flex: 1,
-                  child: Container(
-                    color: Colors.green,
-                  ))
+              Flexible(flex: 1, child: Container())
             ],
           ),
         ));
